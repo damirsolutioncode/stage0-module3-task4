c117a8e75123a28e9f3068000bc4988b895470f0
damirsolutioncode <damir.solutioncode@gmail.com>
2022-10-11 19:21:20 +0500

    The creator of theory not only didn't include any info to be able to solve the IntRepresentation.java, but even didn't bother themselves to paste any relevant links on the topic. Strange approach to teching a programming language

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a = 0;
+        int b = 0;
+        System.out.print((9*a^2-5*b+a-5)*((a+b-4*a*b)/2));
+
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -1,6 +1,41 @@
 package lang.print.gaps.task4;
 
+import java.io.PrintWriter;
+
 public class IntRepresentation {
     public static void main(String[] args) {
+        char chc = 'c';
+        char chh = 'h';
+        char cha = 'a';
+        char chr = 'r';
+        /*
+        int c = c - '0';
+        int h = chh - '0';
+        int a = cha - '0';
+        int r = chr - '0';
+        */
+
+
+        int c = Character.getNumericValue('c');
+        int h = Character.getNumericValue('h');
+        int a = Character.getNumericValue('a');
+        int r = Character.getNumericValue('r');
+
+      /*  int[] myChar = {c, h, a, r};
+
+        Writer writer = new PrintWriter(System.out);
+
+        writer.write(myChar, 0, 4);
+        writer.flush();
+        */
+
+
+
+        System.out.println(c);
+        System.out.println(h);
+        System.out.println(a);
+        System.out.println(r);
+
+
     }
 }
